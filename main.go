f8a1c5f7e08f901e79772148b1a7eb11a45c6f40
Alexander <al.vi.sapelkin@gmail.com>
2017-07-31 00:49:32 +0300

    commets

--- main.go
+++ main.go
@@ -5,22 +5,6 @@ import (
 	"strings"
 )
 
-// func main() {
-
-// 	// `os.Args` provides access to raw command-line
-// 	// arguments. Note that the first value in this slice
-// 	// is the path to the program, and `os.Args[1:]`
-// 	// holds the arguments to the program.
-// 	argsWithProg := os.Args
-// 	argsWithoutProg := os.Args[1:]
-
-// 	// You can get individual args with normal indexing.
-// 	arg := os.Args[1]
-
-// 	fmt.Println(argsWithProg)
-// 	fmt.Println(argsWithoutProg)
-// 	fmt.Println(arg)
-// }
 func check(e error) {
 	if e != nil {
 		panic(e)
